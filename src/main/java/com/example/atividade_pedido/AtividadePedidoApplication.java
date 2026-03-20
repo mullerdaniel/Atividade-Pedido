825a22a2e92bcddf9720299af181b574c840ac85
Daniel Müller <miladaniel370@gmail.com>
2026-03-20 13:48:29 -0300

    Feat: Adicionando estruturação base

---
+++ src/main/java/com/example/atividade_pedido/AtividadePedidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadePedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atividade_pedido/AtividadePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
