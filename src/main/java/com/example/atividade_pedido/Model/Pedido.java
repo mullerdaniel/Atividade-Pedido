238e54647c3a01f143f9208179ef19e3f6cfe566
Daniel Müller <miladaniel370@gmail.com>
2026-03-20 13:54:10 -0300

    Feat: Finalização classe pedido

---
+++ src/main/java/com/example/atividade_pedido/Model/Pedido.java
@@ -0,0 +1,23 @@
+package com.example.atividade_pedido.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Date data_pedido;
+}
